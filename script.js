d5294ecc212aa5d96cec0737b9bac8a526ae0a95
Chirag930929 <chiragchogale2005@gmail.com>
2025-05-21 14:39:44 +0530

    committ
    so many changes

---
+++ script.js
@@ -0,0 +1,5 @@
+// Hamburger toggle
+document.getElementById('hamburgerBtn').addEventListener('click', () => {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('hidden');
+});
